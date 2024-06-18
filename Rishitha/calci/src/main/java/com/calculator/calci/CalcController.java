2ccf02373c2f6401a4e1fc8f4c9c988369af698a
rishitha1910 <80247582+rishitha1910@users.noreply.github.com>
2024-06-18 13:03:01 -0400

    added calci

---
+++ Rishitha/calci/src/main/java/com/calculator/calci/CalcController.java
@@ -0,0 +1,44 @@
+package com.calculator.calci;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class CalcController {
+
+    @PostMapping("/index")
+    public String calculate(@RequestParam double num1,
+                            @RequestParam double num2,
+                            @RequestParam String operation,
+                            Model model) {
+        double result;
+
+        switch (operation) {
+            case "add":
+                result = num1 + num2;
+                break;
+            case "subtract":
+                result = num1 - num2;
+                break;
+            case "multiply":
+                result = num1 * num2;
+                break;
+            case "divide":
+                if (num2 == 0) {
+//                    model.addAttribute("result", "Error: Division by zero");
+                	
+//                    return "result";
+                    return "Error Division by Zero";
+                }
+                result = num1 / num2;
+                break;
+            default:
+                model.addAttribute("result", "Invalid operation");
+                return "result";
+        }
+
+        model.addAttribute("result", result);
+        return "result";
+    }
+}
